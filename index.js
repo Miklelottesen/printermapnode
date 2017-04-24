2510bd381ad3dfa9d1fe5dcf2661a447d29148c2
Miklelottesen <miklelottesen@gmail.com>
2017-04-24 11:44:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express   =    require("express");
+//var express   =    require("express");
 var mysql     =    require('mysql');
 //var app       =    express();
 var server    =    require('http').createServer(onRequest);
